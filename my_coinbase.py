3e27273d74d4cf559c960f11023797f51db3ee05
Pesetas <34454447+Pesetas@users.noreply.github.com>
2020-04-12 15:54:51 +0200

    Update my_coinbase.py

--- my_coinbase.py
+++ my_coinbase.py
@@ -3,13 +3,14 @@ from coinbase.wallet.client import Client
 #Introducid vuestros valores en lugar de las XXXXXXXXXXX
 api_key = 'XXXXXXXXXXXXXX'
 api_secret = 'XXXXXXXXXXXXXXXXXXXXXXX'
+cuenta = 'XXXXXXXXXXXXXXXXXXXXX'
 client = Client(api_key, api_secret)
 
-cambio_actual = client.get_spot_price(currency_pair = "BTC-EUR")
-cambio_actual = cambio_actual["amount"]
-posicion = cambio_actual.find(".")
-cambio_actual = cambio_actual[0:(posicion + 3)]
-print("EUR", cambio_actual, "el BIT")
+def cambio_actual():
+  cambio_actual = client.get_spot_price(currency_pair = "BTC-EUR")["amount"]
+  cambio_actual = cambio_actual[0:(cambio_actual.find(".") + 3)]
+  return "EUR" + cambio_actual
 
-mi_cartera = client.get_account("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-print(mi_cartera["native_balance"], "en la cartera")
+def mi_cartera():
+  mi_cartera = client.get_account(cuenta)
+  return mi_cartera["native_balance"]
